9b23380e4fe6bbb4e6dbe36a5fbecc8bf283e43b
S19120063 <sosa.jeremias.1a@gmail.com>
2024-11-07 14:22:22 -0600

    PRIMEROS CAMBIOS

---
+++ resources/views/front/index1.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<body>
+    <h1>Index</h1>
+    <blockquote class="twitter-tweet"><p lang="es" dir="ltr">Te invitamos a visitar nuestro sitio web COST para acceder a información valiosa sobre costos, presupuestos y más. Aquí compartimos nuestro compromiso con la integridad en la construcción.🏗<br><br>👉<a href="https://t.co/hnMSUtJYMU">https://t.co/hnMSUtJYMU</a> <a href="https://t.co/CtNmgaxlwy">pic.twitter.com/CtNmgaxlwy</a></p>&mdash; CoST Jalisco (@CostJalisco) <a href="https://twitter.com/CostJalisco/status/1758884010594537598?ref_src=twsrc%5Etfw">February 17, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    
+</body>
+</html>
